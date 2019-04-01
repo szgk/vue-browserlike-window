07d44fba230e4717434c9f418f74220457944d1d
szgk <szgk123@gmail.com>
2019-04-01 10:29:20 +0900

    fix: src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import browserlikeWindow from '@/components/vue-browserlike-window.vue'
 import browserlikeWindowItem from '@/components/vue-browserlike-window-item.vue'
 
 export default {
-  install (Vue, options) {
+  install (Vue) {
     Vue.component('vb-window', browserlikeWindow),
     Vue.component('vb-window-item', browserlikeWindowItem)
   }
